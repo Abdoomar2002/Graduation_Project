319e9bd90c534a15102ec07ca03cde0ca2bcd0ba
unknown <rahmabahaa785@gmail.com>
2024-04-05 02:39:02 +0200

    forget RB

---
+++ Hatley/Services/IPasswordResetService.cs
@@ -0,0 +1,10 @@
+﻿namespace Hatley.Services
+{
+    public interface IPasswordResetService
+    {
+        Task<bool> GeneratePasswordResetTokenForUser(string email);
+        Task<bool> GeneratePasswordResetTokenForDelivery(string email);
+        Task<bool> ResetPassword(string email, string token, string newPassword);
+
+    }
+}
